0cb508ee16664514903003941047fd94a2617fd8
Edmilson Soares <esbnet@gmail.com>
2023-05-19 18:00:12 -0300

    feat: :sparkles: config initial route

---
+++ app/_layout.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { styled } from "nativewind";
+import { StatusBar } from "expo-status-bar";
+import { SplashScreen, Stack } from "expo-router";
+
+import { ImageBackground} from "react-native";
+
+import * as SecureStore from 'expo-secure-store'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+
+import blurBg from "../src/assets/bg-blur.png";
+import Stripes from "../src/assets/stripes.svg";
+
+
+const StyledStripes = styled(Stripes);
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint:
+    "https://github.com/settings/connections/applications/f7319650946839d29f4f",
+};
+
+export default function Layout() {
+  const [isUserAuthenticated, setIsAuthenticated] = useState<null | boolean>(null)
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  useEffect(()=>{
+    SecureStore.getItemAsync('st-token-app').then( token =>{
+      setIsAuthenticated(!!token)
+    })
+  },[])
+
+  if (!hasLoadedFonts) return <SplashScreen />;
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 bg-gray-900"
+      imageStyle={{ position: "absolute", left: "-100%" }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <StatusBar style="light" translucent />
+
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: "transparent" },
+        }}
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated}/>
+        <Stack.Screen name="memories"/>
+      </Stack>
+    </ImageBackground>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -6,9 +6,7 @@ import * as SecureStore from "expo-secure-store";
 import { useRouter } from "expo-router";
 
 import {
-  ImageBackground,
   Text,
-  Touchable,
   TouchableOpacity,
   View,
 } from "react-native";
@@ -20,7 +18,6 @@ import {
 } from "@expo-google-fonts/roboto";
 import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
 
-import blurBg from "../src/assets/bg-blur.png";
 import Stripes from "../src/assets/stripes.svg";
 import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
 
@@ -76,13 +73,7 @@ export default function App() {
   if (!hasLoadedFonts) return null;
 
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: "absolute", left: "-100%" }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center  px-8 py-10" >
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
         <View className="space-y-2">
@@ -111,6 +102,6 @@ export default function App() {
       </Text>
 
       <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   );
 }
