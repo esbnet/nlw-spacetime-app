20393f87154ee2829a38b537a6f54600146da59f
Edmilson Soares <esbnet@gmail.com>
2023-05-18 22:41:07 -0300

    refactor: :recycle: refactore entry point and config expo routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,10 @@
+import { styled } from "nativewind";
+import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
+import { useAuthRequest, makeRedirectUri } from "expo-auth-session";
+import * as SecureStore from "expo-secure-store";
+import { useRouter } from "expo-router";
+
 import {
   ImageBackground,
   Text,
@@ -7,42 +13,38 @@ import {
   View,
 } from "react-native";
 
-import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
-
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-
 import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
 
-import blurBg from "./src/assets/bg-blur.png";
+import blurBg from "../src/assets/bg-blur.png";
+import Stripes from "../src/assets/stripes.svg";
+import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
 
-import Stripes from "./src/assets/stripes.svg";
-import NLWLogo from "./src/assets/nlw-spacetime-logo.svg";
-
-import { styled } from "nativewind";
-import { useEffect } from "react";
-import { api } from "./src/lib/api";
+import { api } from "../src/lib/api";
 
 const StyledStripes = styled(Stripes);
 
 // Endpoint
 const discovery = {
-  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
-  tokenEndpoint: 'https://github.com/login/oauth/access_token',
-  revocationEndpoint: 'https://github.com/settings/connections/applications/f7319650946839d29f4f',
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint:
+    "https://github.com/settings/connections/applications/f7319650946839d29f4f",
 };
 
 export default function App() {
+  const router = useRouter();
 
   const [request, response, signInWithGithub] = useAuthRequest(
     {
-      clientId: 'f7319650946839d29f4f',
-      scopes: ['identity'],
+      clientId: "f7319650946839d29f4f",
+      scopes: ["identity"],
       redirectUri: makeRedirectUri({
-        scheme: 'nlwspacetime'
+        scheme: "nlwspacetime",
       }),
     },
     discovery
@@ -54,19 +56,20 @@ export default function App() {
     BaiJamjuree_700Bold,
   });
 
-  useEffect(() => {
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post("/register", { code });
+    const { token } = response.data;
+    await SecureStore.setItemAsync("st-token-app", token);
+    router.push('/memories')
+  }
 
+  useEffect(() => {
     // console.log(makeRedirectUri({
     //   scheme: 'nlwspacetime'
     // }),)
-
-    if (response?.type === 'success') {
+    if (response?.type === "success") {
       const { code } = response.params;
-
-      api.post('/register', {code}).then(response => {
-        const {token}=response.data
-        console.log(token)
-      })
+      handleGithubOAuthCode(code);
     }
   }, [response]);
 
@@ -92,18 +95,20 @@ export default function App() {
           </Text>
         </View>
 
-        <TouchableOpacity 
-        activeOpacity={0.7} 
-        className="rounded-full bg-green-500 px-5 py-2"
-        onPress={()=>signInWithGithub()}
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
-          <Text className="font-alt text-sm uppercase text-black leading-none">
+          <Text className="font-alt text-sm uppercase leading-none text-black">
             Cadastrar lembrança
           </Text>
         </TouchableOpacity>
       </View>
 
-      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">Feito com 💜 no NLW da Rocketseat</Text>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
 
       <StatusBar style="light" translucent />
     </ImageBackground>
